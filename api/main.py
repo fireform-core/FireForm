626c7455aa7bee00132bd07edc19ec8dbd83a39a
marcvergees <marcvergessantiago@gmail.com>
2026-04-15 20:03:12 -0700

    feat: :sparkles: addded a new template when the db is created

--- api/main.py
+++ api/main.py
@@ -1,11 +1,25 @@
+
 import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import forms, templates
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from api.routes import templates, forms
+from api.db.init_db import init_db
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup: Initialize the database and seed it if necessary
+    print("Initializing database...")
+    init_db()
+    yield
+    # Shutdown logic goes here if needed
+
+app = FastAPI(lifespan=lifespan)
 
 default_origins = "http://127.0.0.1:5173"
 allowed_origins = [
