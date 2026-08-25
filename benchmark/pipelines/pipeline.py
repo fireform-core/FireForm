d36ebc5cbf9f57d21b61c0699a3d9096ca1db658
marcvergees <marcvergessantiago@gmail.com>
2026-08-25 20:42:20 +0200

    feat: :sparkles: first approach of implementing pipeline c

--- benchmark/pipelines/pipeline.py
+++ benchmark/pipelines/pipeline.py
@@ -1,5 +1,10 @@
 from benchmark.pipelines.base import BasePipeline, PipelineExtractionOutput
-
+from app.services.input import InputService
+from app.api.routes.input import submit_text_input
+from app.api.schemas.input import TextInputRequest, TextInputResponse
+from app.api.routes.extraction import create_extraction, get_extraction_result
+from app.api.schemas.extraction import ExtractionJobResponse
+import time
 
 class Pipeline(BasePipeline):
     def run(self, narrative: str, template_schema: dict, pdf_path: str) -> PipelineExtractionOutput:
@@ -18,6 +23,29 @@ class Pipeline(BasePipeline):
         >>> confidence = result.field_confidence
         >>> latency = result.latency_seconds
         '''
+
+        # 1. Input Layer
+        text_input_response = submit_text_input(TextInputRequest(narrative=narrative))
+        input_id = text_input_response.input_id
+
+        # 2. Extraction Layer
+        job_response = create_extraction(input_id)
+
+        extract_id = job_response.extract_id
+        
+        # 3. Polling loop (wait 5 seconds between checks until condition is met)
+        extraction_starting_time = time.time()
+        while get_extraction_result(extract_id).status == "processing":
+            time.sleep(5)
+        extraction_ending_time = time.time()
+        extraction_latency = extraction_ending_time - extraction_starting_time
+
+
+        incident_contract = get_extraction_result(extract_id).partial_result
+        
+
+
+
         return PipelineExtractionOutput(
             extracted_fields={},
             field_confidence={},
